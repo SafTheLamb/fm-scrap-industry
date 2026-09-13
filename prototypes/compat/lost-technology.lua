ec8ec66a1b03b7ab355cf0957bfb66bc6798d5e9
Sapphira Riza <safthelamb@gmail.com>
2026-09-12 20:17:04 -0700

    Add Circuit scrap to Lost Technology compat

--- prototypes/compat/lost-technology.lua
+++ prototypes/compat/lost-technology.lua
@@ -11,4 +11,6 @@ if mods["lost-technology"] then
 		fulgoran_metal_smelting.icons[1].icon = "__scrap-industry__/graphics/icons/compat/fulgoran-metal.png"
 	end
 	frep.add_shared_probability_result("lost-scrap-iron-recycling", {type="item", name="iron-scrap", amount=1}, 0.1)
+	frep.add_result("lost-ai-core-recycling", {type="item", name="circuit-scrap", amount=1, independent_probability=0.1}, true, 4)
+	frep.add_result("lost-ai-core-dead-recycling", {type="item", name="circuit-scrap", amount=1, independent_probability=0.1}, true, 4)
 end
