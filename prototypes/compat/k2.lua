027c1ca418b646a3749f14d71d5b3bdc33d2a728
Sapphira Riza <safthelamb@gmail.com>
2026-09-24 23:29:17 -0700

    a bit of k2 compat

---
+++ prototypes/compat/k2.lua
@@ -0,0 +1,4 @@
+if mods["Krastorio2"] or mods["Krastorio2-spaced-out"] then
+	ScrapIndustry.recipes["uranium-rounds-magazine"] = {ignore=true}
+	ScrapIndustry.recipes["kr-gps-satellite"] = {ignore=true}
+end
